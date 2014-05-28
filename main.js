b19eba722f34c6035ec11b67427a6520420c4af1
Justas Brazauskas <brazauskasjustas@gmail.com>
2014-05-28 14:40:05 +0300

    This is now working

--- main.js
+++ main.js
@@ -6,18 +6,81 @@ var fs = require('fs');
 var deps = [];
 var parsed = [];
 var base = path.resolve('.');
+var file;
 
-check([process.argv[2]], base);
+var defaultModules = [
+    'assert',
+    'buffer',
+    'cluster',
+    'domain',
+    'child_process',
+    'crypto',
+    'dns',
+    'events',
+    'fs',
+    'http',
+    'https',
+    'net',
+    'os',
+    'path',
+    'punycode',
+    'querystring',
+    'readline',
+    'string_decoder',
+    'tls',
+    'tty',
+    'dgram',
+    'url',
+    'util',
+    'vm'
+];
+
+if (process.argv.length > 3) {
+
+} else {
+    if (process.argv[2]) {
+        base = path.dirname(path.resolve(base, process.argv[2]));
+        file = path.basename(process.argv[2]);
+    }
+}
+
+if (!fs.existsSync(base + '/package.json')) {
+    console.error('Project must have a package.json file. Aborting');
+    process.exit(1);
+}
+
+var package = JSON.parse(fs.readFileSync(base + '/package.json'));
+var packageDeps = Object.keys(package.dependencies);
+
+go(base, file);
+
 var processedDeps = [];
 
 
 deps.forEach(function(dep) {
-    if(processedDeps.indexOf(dep) === -1) {
-        processedDeps.push(dep);
+    if (processedDeps.indexOf(dep) === -1) {
+        processedDeps.push(dep.toLowerCase());
     }
 });
 
-console.log(processedDeps);
+var dependencies = {
+    unused: [],
+    missing: []
+};
+
+packageDeps.forEach(function(dep) {
+    if (processedDeps.indexOf(dep.toLowerCase()) === -1) {
+        dependencies.unused.push(dep.toLowerCase());
+    }
+});
+
+processedDeps.forEach(function(dep) {
+    if (packageDeps.indexOf(dep.toLowerCase()) === -1 && defaultModules.indexOf(dep.toLowerCase()) === -1) {
+        dependencies.missing.push(dep.toLowerCase());
+    }
+});
+
+console.log(dependencies);
 
 
 function get(items) {
@@ -27,7 +90,7 @@ function get(items) {
     };
     items.forEach(function(dep) {
         if (dep.indexOf('/') === -1 && dep.indexOf('.js') === -1) {
-            files.deps.push(dep);
+            files.deps.push(dep.toLowerCase());
         } else if (dep.indexOf('.json') === -1) {
             files.files.push(dep);
         }
